425927f507571006537b9dffa43638d5d7a5ddac
Insomi <tbcvct@mail.ru>
2020-05-28 19:40:18 +0300

    update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var modalImg = document.getElementById('modal-img');
 
 modal.addEventListener('click', function(event) {
   if (event.target === modal) {
-    modal.classList.add('closed');
+  modal.classList.add('closed');
   }
 })
 
@@ -17,8 +17,8 @@ function loadUsers() {
   var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', 'https://json.medrating.org/users/');
   ourRequest.onload = function() {
-      var users = JSON.parse(ourRequest.responseText);
-      renderUsers(users);
+  var users = JSON.parse(ourRequest.responseText);
+  renderUsers(users);
   };
   ourRequest.send();
 }
@@ -27,8 +27,8 @@ function loadUserAlbums(userId, container) {
   var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', 'https://json.medrating.org/albums/?userId=' + userId);
   ourRequest.onload = function() {
-      var albums = JSON.parse(ourRequest.responseText);
-      renderUserAlbums(albums, container);
+  var albums = JSON.parse(ourRequest.responseText);
+  renderUserAlbums(albums, container);
   };
   ourRequest.send();
 }
@@ -37,15 +37,15 @@ function loadUserPhotos(albumId, container) {
    var ourRequest = new XMLHttpRequest();
    ourRequest.open('GET', 'https://json.medrating.org/photos/?albumId=' + albumId);
    ourRequest.onload = function() {
-      var photos = JSON.parse(ourRequest.responseText);
-      renderUserPhotos(photos, container);
-  };
-  ourRequest.send();
+   var photos = JSON.parse(ourRequest.responseText);
+   renderUserPhotos(photos, container);
+   };
+   ourRequest.send();
 }
 
 function onUserClick(id, event) {
-  var containerElement = event.target.parentNode;
-  var isOpen = containerElement.classList.contains('opened');
+   var containerElement = event.target.parentNode;
+   var isOpen = containerElement.classList.contains('opened');
   
   if (isOpen) {
     containerElement.classList.remove('opened');
@@ -57,7 +57,16 @@ function onUserClick(id, event) {
 }
 
 function onAlbumClick(id, event) {
-  loadUserPhotos(id, event.target.parentNode);
+  var containerElement = event.target.parentNode;
+  var isOpen = containerElement.classList.contains('opened');
+  
+  if (isOpen) {
+    containerElement.classList.remove('opened');
+    containerElement.getElementsByClassName('photos')[0].innerHTML = '';
+  } else {
+    containerElement.classList.add('opened');
+    loadUserPhotos(id, containerElement);
+  }
 }
 
 function onPhotosClick(src) {
@@ -96,8 +105,30 @@ function renderUserPhotos(data, container) {
     var title = data[i].title ? data[i].title : 'None';
     var url= data[i] ? data[i].url : null;
     var id = data[i].id;
-    content += "<li class='photo'><img width='30px' onClick='onPhotosClick(\"" + url + "\")' src='" + url + "' /></li>";
+    content += "<li class='photo'><img width='70px' onClick='onPhotosClick(\"" + url + "\")' src='" + url + "' /></li>";
   }
 
   container.getElementsByClassName('photos')[0].insertAdjacentHTML('beforeend', content);
 }
+
+/*
+var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+favorites.forEach(function(favorite) {
+  document.getElementById(favorite).className = 'fav';
+});
+document.querySelector('.photo').addEventListener('click', function(e) {
+  var id = e.target.src,
+      item = e.target,
+      index = favorites.indexOf(src);
+  if (!src) return;
+  if (index == -1) {
+    favorites.push(src);
+    item.className = 'fav';
+  } else {
+    favorites.splice(index, 1);
+    item.className = '';
+  }
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+});
+*/
+//https://jsfiddle.net/farhadB/ocgcejg2/
